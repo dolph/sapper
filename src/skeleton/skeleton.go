0c50f18bd3ca1261640cce658d5a772ad3262b19
Dolph Mathews <dolph.mathews@gmail.com>
2017-05-03 21:38:54 -0500

    Convert CLI app to tested web service

--- src/skeleton/skeleton.go
+++ src/skeleton/skeleton.go
@@ -1,7 +1,41 @@
-package main
+package skeleton
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+    "google.golang.org/appengine"
+)
+
+func init() {
+    http.HandleFunc("/", IndexHandler)
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+    switch r.URL.Path {
+        case "/":
+            switch r.Method {
+                case http.MethodGet:
+                    GetIndex(w, r)
+                default:
+                    NotFound(w, r)
+            }
+        default:
+            NotFound(w, r)
+    }
+}
+
+func GetIndex(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprint(w, "hello, world")
+}
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusNotFound)
+    w.Header().Set("Content-Type", "text/plain")
+    fmt.Fprintf(w, "%d Not Found", http.StatusNotFound)
+}
 
 func main() {
-    fmt.Printf("hello, world\n")
+    appengine.Main()
 }
---
+++ src/skeleton/skeleton_test.go
@@ -0,0 +1,97 @@
+package skeleton
+
+import (
+    "testing"
+    "net/http"
+    "net/http/httptest"
+    "strings"
+)
+
+type TestHandler struct {
+    t *testing.T
+
+    // The HTTP handler function to be tested.
+    f func (http.ResponseWriter, *http.Request)
+}
+
+// Build an HTTP request, pass it to the HTTP handler, and return the response.
+func (handler TestHandler) request(method string, path string, headers map[string]string) TestResponse {
+    request, err := http.NewRequest(method, path, nil)
+    if err != nil {
+        handler.t.Fatal(err)
+    }
+
+    // Set request headers, if any.
+    for header, value := range headers {
+        request.Header.Set(header, value)
+    }
+
+    response := httptest.NewRecorder()
+    http.HandlerFunc(handler.f).ServeHTTP(response, request)
+    return TestResponse{handler.t, response}
+}
+
+// Make a GET request to the HTTP handler, and return the response.
+func (handler TestHandler) Get(path string, headers map[string]string) TestResponse {
+    return handler.request(http.MethodGet, path, headers)
+}
+
+// Make a POST request to the HTTP handler, and return the response.
+func (handler TestHandler) Post(path string, headers map[string]string) TestResponse {
+    return handler.request(http.MethodPost, path, headers)
+}
+
+type TestResponse struct {
+    t *testing.T
+
+    // The HTTP response being asserted against.
+    r *httptest.ResponseRecorder
+}
+
+// Ensure that the response contains the expected status code.
+func (response TestResponse) AssertStatusEquals(expected int) {
+    if response.r.Code != expected {
+        response.t.Errorf(
+            "Handler returned unexpected status code: got `%v` want `%v`",
+            response.r.Code, expected)
+    }
+}
+
+// Ensure that the response body is exactly as expected.
+func (response TestResponse) AssertBodyEquals(expected string) {
+    if actual := response.r.Body.String(); actual != expected {
+        response.t.Errorf(
+            "Handler returned unexpected body: got `%v` want `%v`",
+            actual, expected)
+    }
+}
+
+// Ensure that the response body contains a substring.
+func (response TestResponse) AssertBodyContains(substr string) {
+    if actual := response.r.Body.String(); !strings.Contains(actual, substr) {
+        response.t.Errorf(
+            "Handler returned unexpected body: did not find `%v` in `%v`",
+            substr, actual)
+    }
+}
+
+// Ensure that the response contains a specific header.
+func (response TestResponse) AssertHeaderExists(header string) {
+    if _, ok := response.r.Header()[header]; !ok {
+        response.t.Errorf(
+            "Handler did not set header `%v`",
+            header)
+    }
+}
+
+func TestGetIndex(t *testing.T) {
+    response := TestHandler{t, IndexHandler}.Get("/", nil)
+    response.AssertStatusEquals(http.StatusOK)
+    response.AssertBodyEquals("hello, world")
+}
+
+func TestGetInvalidUrl(t *testing.T) {
+    response := TestHandler{t, IndexHandler}.Get("/non-existant", nil)
+    response.AssertStatusEquals(http.StatusNotFound)
+    response.AssertBodyEquals("404 Not Found")
+}
