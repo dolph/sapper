132ac29ea63f141af35ef0e23b55373e330b2c3e
Dolph Mathews <dolph.mathews@gmail.com>
2017-05-16 18:57:01 -0500

    Add a linting step based on go fmt

--- ci/test.sh
+++ ci/test.sh
@@ -5,4 +5,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Run unit tests.
 cd $DIR/../src/skeleton/
+
+# Linting.
+files_needing_linting=$(gofmt -l .)
+if [[ $(gofmt -l .) ]] then
+    echo $files_needing_linting
+    echo "Resolve differences with go fmt."
+    exit 1
+fi
+
 go test
