a95a7bb9bd112d076ca3acbc277f854cd7b80313
Dolph Mathews <dolph.mathews@gmail.com>
2017-05-16 19:00:17 -0500

    Format code with go fmt

--- src/skeleton/main.go
+++ src/skeleton/main.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strings"
-    "google.golang.org/appengine"
+	"fmt"
+	"google.golang.org/appengine"
+	"net/http"
+	"strings"
 )
 
 // Trivially routes requests to the correct response function.
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-    switch r.URL.Path {
-        case "/":
-            switch r.Method {
-                case http.MethodGet:
-                    GetIndex(w, r)
-                default:
-                    NotFound(w, r)
-            }
-        default:
-            NotFound(w, r)
-    }
+	switch r.URL.Path {
+	case "/":
+		switch r.Method {
+		case http.MethodGet:
+			GetIndex(w, r)
+		default:
+			NotFound(w, r)
+		}
+	default:
+		NotFound(w, r)
+	}
 }
 
 // Returns the client's IP address.
 func GetIndex(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
-    w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
 
-    // RemoteAddr is formatted as host:port, so we just trim off the port here
-    // and return the IP.
-    fmt.Fprintf(w, "%s\n", strings.Split(r.RemoteAddr, ":")[0])
+	// RemoteAddr is formatted as host:port, so we just trim off the port here
+	// and return the IP.
+	fmt.Fprintf(w, "%s\n", strings.Split(r.RemoteAddr, ":")[0])
 }
 
 // Returns a 404 Not Found page.
 func NotFound(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
-    w.WriteHeader(http.StatusNotFound)
-    fmt.Fprintf(w, "%d Not Found\n", http.StatusNotFound)
+	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprintf(w, "%d Not Found\n", http.StatusNotFound)
 }
 
 // appengine.Main() expects packages to register HTTP handlers in their init()
 // functions.
 func init() {
-    http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/", IndexHandler)
 }
 
 func main() {
-    // Starts listening on port 8080 (or $PORT), and never returns.
-    // https://godoc.org/google.golang.org/appengine#Main
-    appengine.Main()
+	// Starts listening on port 8080 (or $PORT), and never returns.
+	// https://godoc.org/google.golang.org/appengine#Main
+	appengine.Main()
 }
--- src/skeleton/main_test.go
+++ src/skeleton/main_test.go
@@ -1,117 +1,117 @@
 package main
 
 import (
-    "testing"
-    "net/http"
-    "net/http/httptest"
-    "strings"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 )
 
 type TestHandler struct {
-    t *testing.T
+	t *testing.T
 
-    // The HTTP handler function to be tested.
-    f func (http.ResponseWriter, *http.Request)
+	// The HTTP handler function to be tested.
+	f func(http.ResponseWriter, *http.Request)
 }
 
 // Build an HTTP request, pass it to the HTTP handler, and return the response.
 func (handler TestHandler) request(method, path string, headers map[string]string) TestResponse {
-    request, err := http.NewRequest(method, path, nil)
-    if err != nil {
-        handler.t.Fatal(err)
-    }
+	request, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		handler.t.Fatal(err)
+	}
 
-    // Set request headers, if any.
-    for header, value := range headers {
-        request.Header.Set(header, value)
-    }
+	// Set request headers, if any.
+	for header, value := range headers {
+		request.Header.Set(header, value)
+	}
 
-    // Set an arbitrary remote address for logging purposes, etc.
-    request.RemoteAddr = "1.2.3.4"
+	// Set an arbitrary remote address for logging purposes, etc.
+	request.RemoteAddr = "1.2.3.4"
 
-    response := httptest.NewRecorder()
-    http.HandlerFunc(handler.f).ServeHTTP(response, request)
-    return TestResponse{handler.t, response}
+	response := httptest.NewRecorder()
+	http.HandlerFunc(handler.f).ServeHTTP(response, request)
+	return TestResponse{handler.t, response}
 }
 
 // Make a GET request to the HTTP handler, and return the response.
 func (handler TestHandler) Get(path string, headers map[string]string) TestResponse {
-    return handler.request(http.MethodGet, path, headers)
+	return handler.request(http.MethodGet, path, headers)
 }
 
 // Make a POST request to the HTTP handler, and return the response.
 func (handler TestHandler) Post(path string, headers map[string]string) TestResponse {
-    return handler.request(http.MethodPost, path, headers)
+	return handler.request(http.MethodPost, path, headers)
 }
 
 type TestResponse struct {
-    t *testing.T
+	t *testing.T
 
-    // The HTTP response being asserted against.
-    r *httptest.ResponseRecorder
+	// The HTTP response being asserted against.
+	r *httptest.ResponseRecorder
 }
 
 // Ensure that the response contains the expected status code.
 func (response TestResponse) AssertStatusEquals(expected int) {
-    if response.r.Code != expected {
-        response.t.Errorf(
-            "Handler returned unexpected status code: got `%v` want `%v`",
-            response.r.Code, expected)
-    }
+	if response.r.Code != expected {
+		response.t.Errorf(
+			"Handler returned unexpected status code: got `%v` want `%v`",
+			response.r.Code, expected)
+	}
 }
 
 // Ensure that the response body is exactly as expected.
 func (response TestResponse) AssertBodyEquals(expected string) {
-    if actual := response.r.Body.String(); actual != expected {
-        response.t.Errorf(
-            "Handler returned unexpected body: got `%v` want `%v`",
-            actual, expected)
-    }
+	if actual := response.r.Body.String(); actual != expected {
+		response.t.Errorf(
+			"Handler returned unexpected body: got `%v` want `%v`",
+			actual, expected)
+	}
 }
 
 // Ensure that the response body contains a substring.
 func (response TestResponse) AssertBodyContains(substr string) {
-    if actual := response.r.Body.String(); !strings.Contains(actual, substr) {
-        response.t.Errorf(
-            "Handler returned unexpected body: did not find `%v` in `%v`",
-            substr, actual)
-    }
+	if actual := response.r.Body.String(); !strings.Contains(actual, substr) {
+		response.t.Errorf(
+			"Handler returned unexpected body: did not find `%v` in `%v`",
+			substr, actual)
+	}
 }
 
 // Ensure that the response contains a specific header.
 func (response TestResponse) AssertHeaderExists(header string) {
-    if _, ok := response.r.Header()[header]; !ok {
-        response.t.Errorf(
-            "Handler did not set header `%v`",
-            header)
-    }
+	if _, ok := response.r.Header()[header]; !ok {
+		response.t.Errorf(
+			"Handler did not set header `%v`",
+			header)
+	}
 }
 
 // Ensure that the response contains a specific header-value pair.
 func (response TestResponse) AssertHeaderContains(header, expected string) {
-    response.AssertHeaderExists(header)
-    actuals, _ := response.r.Header()[header]
-    for _, actual := range actuals {
-        if actual == expected {
-            return
-        }
-    }
+	response.AssertHeaderExists(header)
+	actuals, _ := response.r.Header()[header]
+	for _, actual := range actuals {
+		if actual == expected {
+			return
+		}
+	}
 
-    response.t.Errorf(
-        "Handler returned unexpected %v: got `%v` want `%v`",
-        header, actuals, expected)
+	response.t.Errorf(
+		"Handler returned unexpected %v: got `%v` want `%v`",
+		header, actuals, expected)
 }
 
 func TestGetIndex(t *testing.T) {
-    response := TestHandler{t, IndexHandler}.Get("/", nil)
-    response.AssertStatusEquals(http.StatusOK)
-    response.AssertBodyEquals("1.2.3.4\n")
-    response.AssertHeaderContains("Content-Type", "text/plain; charset=UTF-8")
+	response := TestHandler{t, IndexHandler}.Get("/", nil)
+	response.AssertStatusEquals(http.StatusOK)
+	response.AssertBodyEquals("1.2.3.4\n")
+	response.AssertHeaderContains("Content-Type", "text/plain; charset=UTF-8")
 }
 
 func TestGetInvalidUrl(t *testing.T) {
-    response := TestHandler{t, IndexHandler}.Get("/non-existant", nil)
-    response.AssertStatusEquals(http.StatusNotFound)
-    response.AssertBodyEquals("404 Not Found\n")
-    response.AssertHeaderContains("Content-Type", "text/plain; charset=UTF-8")
+	response := TestHandler{t, IndexHandler}.Get("/non-existant", nil)
+	response.AssertStatusEquals(http.StatusNotFound)
+	response.AssertBodyEquals("404 Not Found\n")
+	response.AssertHeaderContains("Content-Type", "text/plain; charset=UTF-8")
 }
