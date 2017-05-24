9d0381e7f6eb50e8c624518eab106c4cff445811
Dolph Mathews <dolph.mathews@gmail.com>
2017-05-24 11:33:01 -0500

    Separate out HTTP handlers

--- main.go
+++ main.go
@@ -1,40 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 
 	// Google App Engine
 	"google.golang.org/appengine"
 )
-
-// Returns the client's IP address.
-func GetIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-
-	// RemoteAddr is formatted as host:port, so we just trim off the port here
-	// and return the IP.
-	var ip string
-	switch strings.Count(r.RemoteAddr, ":") {
-	case 1:
-		// IPv4 addresses may be of the form IP:port
-		index := strings.LastIndex(r.RemoteAddr, ":")
-		ip = r.RemoteAddr[:index]
-	default:
-		// IPv6 addresses have multiple colons, and no ports.
-		ip = r.RemoteAddr
-	}
-	fmt.Fprintf(w, "%s\n", ip)
-}
-
-// Returns a 404 Not Found page.
-func NotFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprintf(w, "%d Not Found\n", http.StatusNotFound)
-}
 
 // appengine.Main() expects packages to register HTTP handlers in their init()
 // functions.
