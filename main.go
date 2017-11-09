5507de9e44fcc549bc6f74cdb422db543595c17e
Dolph Mathews <dolph.mathews@gmail.com>
2017-11-09 17:33:32 -0600

    Listing users with authentication

--- main.go
+++ main.go
@@ -1,25 +1,91 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strings"
 
 	"gopkg.in/urfave/cli.v1"
 )
+
+type WordPressClient struct {
+	endpoint string
+	cookie   string
+}
+
+func (wp WordPressClient) Get(path string) (string, error) {
+	client := &http.Client{}
+
+	endpointUrl, err := url.Parse(wp.endpoint)
+	if err != nil {
+		log.Fatal(err)
+		return "", err
+	}
+	// Remove a trailing slash from the URL, if any, as we'll assume it's missing later.
+	endpointUrl.Path = strings.TrimRight(endpointUrl.Path, "/")
+	endpointUrl.Path = endpointUrl.Path + path
+
+	// Build the HTTP request.
+	fmt.Println("GET", endpointUrl.String())
+	req, err := http.NewRequest("GET", endpointUrl.String(), nil)
+	if err != nil {
+		log.Fatal("Error building HTTP request for endpoint:", err)
+		return "", err
+	}
+	req.Header.Set("Cookie", wp.cookie)
+
+	// Send the HTTP request.
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error making HTTP request:", err)
+		return "", err
+	}
+
+	// Read the HTTP response.
+	responseData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("Error reading response body", err)
+		return "", err
+	}
+
+	// Format the JSON response to be human-readable.
+	var prettyJSON bytes.Buffer
+	err = json.Indent(&prettyJSON, responseData, "", "  ")
+	if err != nil {
+		log.Fatal("Error parsing JSON: ", err)
+		return "", err
+	}
+
+	return string(prettyJSON.Bytes()), nil
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "sapper"
 	app.Usage = "A client for WordPress."
 
-	wordpress_endpoint := os.Getenv("WORDPRESS_ENDPOINT")
-	wordpress_cookie := os.Getenv("WORDPRESS_COOKIE")
+	wpClient := WordPressClient{
+		endpoint: os.Getenv("WORDPRESS_ENDPOINT"),
+		cookie:   os.Getenv("WORDPRESS_COOKIE"),
+	}
+
+	fmt.Println(wpClient.Get("/wp/v2/users"))
 
 	app.Action = func(c *cli.Context) error {
-		fmt.Println(wordpress_endpoint)
-		fmt.Println(wordpress_cookie)
 		return nil
 	}
+
+	// List users
+	// https://developer.ibm.com/code/wp-json/wp/v2/users
+
+	// Update a user
+	// Docs: https://developer.wordpress.org/rest-api/reference/users/#update-a-user
 
 	app.Run(os.Args)
 }
