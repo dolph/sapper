88e15de970807bf342f52b3adebb23909181a475
Dolph Mathews <dolph.mathews@gmail.com>
2017-11-09 20:55:43 -0600

    Use a list

---
+++ ci/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ex
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../
+
+# Install requirements.
+go get "gopkg.in/urfave/cli.v1"
+
+# Run unit tests.
+go run
